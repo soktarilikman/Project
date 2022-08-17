f88675300f2163c7307e91c3b6e64cd9048b802d
Mexriddinov <109057913+Mexriddinov@users.noreply.github.com>
2022-08-18 02:14:23 +0500

    -ismim

--- app/src/main/java/uz/gita/project/MainActivity.kt
+++ app/src/main/java/uz/gita/project/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // Qalay endi bollar
+
+        //Muhammadali
     }
 }
